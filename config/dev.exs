85a191f760e5b8a4cda350994612d3a7ba6ff377
Thomas Støa Pettersen <thomas@forgetit.no>
2020-01-22 09:27:48 +0100

    Disable debug errors for dev env

--- config/dev.exs
+++ config/dev.exs
@@ -17,7 +17,7 @@ config :todo_backend_phoenix, TodoBackendPhoenix.Repo,
 # with webpack to recompile .js and .css sources.
 config :todo_backend_phoenix, TodoBackendPhoenixWeb.Endpoint,
   http: [port: 4000],
-  debug_errors: true,
+  debug_errors: false,
   code_reloader: true,
   check_origin: false,
   watchers: [
